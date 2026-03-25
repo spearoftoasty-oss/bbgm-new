e919d44f542f52df7fff0d27638577a9b429eda8
unknown <spearoftoasty@gmail.com>
2026-03-25 10:11:54 +0800

    bbgm 1.3

--- src/worker/core/player/archetypes.ts
+++ src/worker/core/player/archetypes.ts
@@ -24,128 +24,128 @@ export const archetypeModifiers: Record<
 > = {
 	// --- PLAYMAKING CORE ---
 	facilitator: {
-		oiq: 0.3,
-		pss: 0.15,
-		drb: 0.1,
-		spd: 0.05,
+		oiq: 1.3,
+		pss: 1.15,
+		drb: 1.1,
+		spd: 1.05,
 	},
 
 	playmaker: {
-		pss: 0.25,
-		drb: 0.2,
-		oiq: 0.15,
+		pss: 1.25,
+		drb: 1.2,
+		oiq: 1.15,
 	},
 
 	// --- SCORING TYPES ---
 	shotCreator: {
-		fg: 0.2,
-		tp: 0.1,
-		drb: 0.2,
-		oiq: 0.1,
+		fg: 1.2,
+		tp: 1.1,
+		drb: 1.2,
+		oiq: 1.1,
 	},
 
 	offBallScorer: {
-		fg: 0.1,
-		tp: 0.2,
-		oiq: 0.1,
-		spd: 0.2,
+		fg: 1.1,
+		tp: 1.2,
+		oiq: 1.1,
+		spd: 1.2,
 	},
 
 	threeAndD: {
-		tp: 0.3,
+		tp: 1.3,
 		diq: 1.2,
 		oiq: 1.1,
 	},
 
 	sharpshooter: {
-		fg: 0.15,
-		tp: 0.35,
-		ft: 0.1,
+		fg: 1.15,
+		tp: 1.35,
+		ft: 1.1,
 	},
 
 	slasher: {
-		spd: 0.1,
-		jmp: 0.1,
-		dnk: 0.2,
-		ins: 0.15,
-		drb: 0.05,
+		spd: 1.1,
+		jmp: 1.1,
+		dnk: 1.2,
+		ins: 1.15,
+		drb: 1.05,
 	},
 
 	// --- BIG / INTERIOR TYPES ---
 	postScorer: {
-		ins: 0.3,
-		stre: 0.2,
-		reb: 0.1,
+		ins: 1.3,
+		stre: 1.2,
+		reb: 1.1,
 	},
 
 	athleticFinisher: {
-		dnk: 0.2,
-		jmp: 0.1,
-		spd: 0.1,
-		ins: 0.2,
+		dnk: 1.2,
+		jmp: 1.1,
+		spd: 1.1,
+		ins: 1.2,
 	},
 
 	rebounder: {
-		reb: 0.35,
-		stre: 0.15,
-		jmp: 0.1,
+		reb: 1.35,
+		stre: 1.15,
+		jmp: 1.1,
 	},
 
 	physicalEnforcer: {
-		stre: 0.3,
-		ins: 0.2,
-		reb: 0.1,
-		diq: 0.1,
+		stre: 1.3,
+		ins: 1.2,
+		reb: 1.1,
+		diq: 1.1,
 	},
 
 	// --- DEFENSE TYPES ---
 	perimeterDefender: {
-		diq: 0.3,
-		spd: 0.25,
-		drb: 0.05,
+		diq: 1.3,
+		spd: 1.25,
+		drb: 1.05,
 	},
 
 	interiorDefender: {
-		diq: 0.25,
-		stre: 0.2,
-		reb: 0.15,
+		diq: 1.25,
+		stre: 1.2,
+		reb: 1.15,
 	},
 
 	rimProtector: {
-		diq: 0.3,
-		reb: 0.2,
-		jmp: 0.1,
+		diq: 1.3,
+		reb: 1.2,
+		jmp: 1.1,
 	},
 
 	switchDefender: {
-		diq: 0.2,
-		spd: 0.2,
-		drb: 0.05,
-		jmp: 0.15,
+		diq: 1.2,
+		spd: 1.2,
+		drb: 1.05,
+		jmp: 1.15,
 	},
 
 	hustler: {
-		oiq: 0.1,
-		diq: 0.1,
-		reb: 0.1,
-		spd: 0.05,
-		endu: 0.25,
+		oiq: 1.1,
+		diq: 1.1,
+		reb: 1.1,
+		spd: 1.05,
+		endu: 1.25,
 	},
 
 	// --- HYBRIDS ---
 	twoWay: {
-		diq: 0.2,
-		oiq: 0.2,
-		fg: 0.1,
-		tp: 0.1,
+		diq: 1.2,
+		oiq: 1.2,
+		fg: 1.1,
+		tp: 1.1,
 	},
 
 	allRounder: {
-		stre: 0.1,
-		spd: 0.1,
-		jmp: 0.1,
-		oiq: 0.1,
-		pss: 0.1,
-		reb: 0.1,
+		stre: 1.1,
+		spd: 1.1,
+		jmp: 1.1,
+		oiq: 1.1,
+		pss: 1.1,
+		reb: 1.1,
 	},
 };
